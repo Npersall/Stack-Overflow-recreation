dc25a9a2a0f5437c4f6585ebb503d6f83ce5d3f5
npersall <npersall@gmail.com>
2017-07-28 16:11:56 -0700

    Add users and login controllers and views.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+get '/users/new' do
+  erb :"users/new"
+end
+
+post '/users' do
+  @user= User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect "users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  check_permissions
+  @user = User.find(params[:id])
+  if current_user && logged_in?
+    erb :'users/show'
+  else
+    redirect erb :'users/new'
+  end
+end
+
