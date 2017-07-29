a858beb2a2dda0994b43036e5e40548a0ccd59fd
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-07-28 17:05:43 -0700

    UPDATE questions controller with route handlers for new comment form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,8 +4,19 @@ get '/questions/:id' do
   erb :'/questions/show', locals: {question: @question}
 end
 
-# get '/questions/:id/comments' do
-#   @question = Question.find(params[:id])
+get '/questions/:id/comments' do
+  @question = Question.find(params[:id])
+  @new_comment = true
 
-#   erb :'/questions/show', locals: {question: @question}
-# end
+  erb :'/questions/show', locals: {question: @question}
+end
+
+post '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.comments.create!({
+    commenter_id: current_user.id,
+    body: params[:new_comment]
+  })
+
+  redirect "/questions/#{ @question.id }"
+end
