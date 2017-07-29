ab038d85077281197be11563560bcfc514624a79
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-07-28 17:47:40 -0700

    UPDATE questions controller route handler for adding new comments to either question or answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,19 +4,35 @@ get '/questions/:id' do
   erb :'/questions/show', locals: {question: @question}
 end
 
-get '/questions/:id/comments' do
+get '/questions/:id/question/comments' do
   @question = Question.find(params[:id])
-  @new_comment = true
+  @question_new_comment = true
 
   erb :'/questions/show', locals: {question: @question}
 end
 
 post '/questions/:id' do
   @question = Question.find(params[:id])
-  @question.comments.create!({
+  @answer = @question.answers.find(params[:answer_id])
+
+  if @answer
+    @answer.comments.create!({
     commenter_id: current_user.id,
     body: params[:new_comment]
   })
-
+  else
+    @question.comments.create!({
+      commenter_id: current_user.id,
+      body: params[:new_comment]
+    })
+  end
   redirect "/questions/#{ @question.id }"
+end
+
+get '/questions/:question_id/answers/:answer_id/comments' do
+  @question = Question.find(params[:question_id])
+  @answer_new_comment = true
+  @this_answer = @question.answers.find(params[:answer_id])
+
+  erb :'/questions/show', locals: {question: @question, this_answer: @this_answer}
 end
