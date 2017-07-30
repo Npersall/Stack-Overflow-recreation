9a10112f153d72674b6e7e707ca83de7adc8d6de
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-07-29 20:25:35 -0700

    UPDATE application.js with event listeners for all vote buttons

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   postQuestionListener();
   newQuestionSubmitListener();
+  questionUpvoteListener();
+  questionDownvoteListener();
+  answerUpvoteListener();
+  answerDownvoteListener();
 });
 
 var postQuestionListener = function() {
@@ -47,7 +51,81 @@ var newQuestionSubmitListener = function() {
       $('#post_question').show();
       $('#new_question_form').trigger('reset')
       $('.questions_posted_list').append(response)
-
     })
   })
 }
+
+
+var questionUpvoteListener = function() {
+  $('#question-upvote').on('click', '#question-upvote-button', function(e){
+    e.preventDefault();
+
+    var form = $(this).closest('#question-upvote');
+
+    questionVoteListener(form);
+  });
+};
+
+
+var questionDownvoteListener = function() {
+  $('#question-downvote').on('click', '#question-downvote-button', function(e){
+    e.preventDefault();
+
+    var form = $(this).closest('#question-downvote');
+
+    questionVoteListener(form);
+  });
+};
+
+var answerUpvoteListener = function() {
+  $('.answers-container').on('click', '#answer-upvote-button', function(e){
+    e.preventDefault();
+
+    var form = $(this).closest('#answer-upvote');
+
+    answerVoteListener(form)
+  });
+};
+
+var answerDownvoteListener = function() {
+  $('.answers-container').on('click', '#answer-downvote-button', function(e){
+    e.preventDefault();
+
+    var form = $(this).closest('#answer-downvote');
+
+    answerVoteListener(form);
+  });
+};
+
+function questionVoteListener(form) {
+  var request = $.ajax({
+    url: form.attr('action'),
+    method: form.attr('method'),
+    data: form.serialize()
+  })
+
+  request.done(function(response) {
+    $('#question-vote-count').html(response);
+  });
+
+  request.fail(function() {
+    alert("Failed to vote");
+  });
+};
+
+function answerVoteListener(form) {
+  var request = $.ajax({
+    url: form.attr('action'),
+    method: form.attr('method'),
+    data: form.serialize()
+  })
+
+  request.done(function(response) {
+    var answer_id = response['answer_id']
+    $('#' + answer_id + ' .answer-vote-count').html(response['vote_count']);
+  });
+
+  request.fail(function() {
+    alert("Failed to vote");
+  });
+};
